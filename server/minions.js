d0a7122edffbd3181424f64512e83a4a39fa04aa
filippopendyk <filipos28511@gmail.com>
2023-04-05 12:38:24 -0400

    creates middleware for minionid param and get req

--- server/minions.js
+++ server/minions.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const { getAllFromDatabase } = require('./db');
+const { getAllFromDatabase, getFromDatabaseById } = require('./db');
 const minionRouter = express.Router();
+
+minionRouter.param('minionId', (req, res, next, minionId) => {
+    const minion = getFromDatabaseById('minions', minionId);
+    if(minion){
+        req.minion = minion;
+        next();
+    } else {
+        return res.status(404).send();
+    }
+})
 
 minionRouter.get('/', (req, res, next) => {
     const minions = getAllFromDatabase('minions')
     res.send(minions);
+})
+
+minionRouter.get('/:minionId', (req, res, next) => {
+    const minion = req.minion;
+    res.send(minion);
 })
 
 module.exports = minionRouter;
