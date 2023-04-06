5aab272fa193512c94b5dad1c9e0eb02a3c3c7ea
filippopendyk <filipos28511@gmail.com>
2023-04-06 13:47:27 -0400

    handles ideaid param

--- server/ideas.js
+++ server/ideas.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const { getAllFromDatabase } = require('./db');
+const { getAllFromDatabase, getFromDatabaseById } = require('./db');
 const ideaRouter = express.Router();
+
+ideaRouter.param(':ideaId', (req, res, next, id) => {
+    const idea = getFromDatabaseById('ideas', id);
+    if(idea){
+        req.idea = idea;
+        next();
+    } else {
+        return res.status(404).send();
+    }
+})
 
 ideaRouter.get('/', (req, res, next) => {
     const ideas = getAllFromDatabase('ideas');
